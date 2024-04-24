bd8b9fb304cd87c868524b9cad12bf5e526bef6a
ArazHMMTV <tu7R7YDYS@code.edu.az>
2024-04-24 11:15:15 +0400

    Fix line endings in .gitignore

---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialzationTask
+{
+    public class Product : ISerializable
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        
+
+        
+
+        public  void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("name", Name,typeof(Product));
+            info.AddValue("id", Id, typeof(Product));
+            info.AddValue("description", Description, typeof(Product));
+            info.AddValue("category", Category, typeof(Product));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace SerialzationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            //product.Id = 1;
+            //product.Name="Test";
+            //product.Description = "Test";
+            //product.Category = "Test";
+            //ProductToJson(product);
+            Product product1 = JsonToProduct(product);
+            Console.WriteLine(product1.Name);
+            Console.WriteLine(product1.Description);
+            Console.WriteLine(product1.Category);
+            Console.WriteLine(product1.Id);
+
+        }
+        public static void ProductToJson(Product product)
+        {
+            string path = "C:\\Users\\Administrator\\Desktop\\file.txt";
+
+            string jsonString = JsonSerializer.Serialize(product);
+
+            File.WriteAllText(path, jsonString);
+
+            Console.WriteLine(path);
+
+        }
+        public static Product JsonToProduct(Product product)
+        {
+            string path = "C:\\Users\\Administrator\\Desktop\\file.txt";
+
+            string jsonString = File.ReadAllText(path);
+
+            return JsonSerializer.Deserialize<Product>(jsonString);
+
+        }
+    }
+}
